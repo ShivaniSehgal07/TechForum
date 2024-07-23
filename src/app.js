09a11c3aaf5fd9269a0d17d25bca93d80d67bf44
Shivani Sehgal <sehgalshivani07@gmail..com>
2024-07-23 12:01:54 -0400

    Removed duplicated code

--- src/app.js
+++ src/app.js
@@ -43,30 +43,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Express session
-app.use(
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect flash
-app.use(flash());
-
-// Global variables
-app.use((req, res, next) => {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
-  next();
-});
-
 home(app);
 app.use(postRoutes);
 app.use(authRoutes);
