1951e13d781dfe2fb1ac27f65656335e81b8eb86
Deneb0872334 <c0872334@mylambton.ca>
2024-07-28 23:49:06 -0400

    Added Profile
    - Added the profile.ejs
    - Added the action link to profile in the header.ejs
    - Added authRoutes.("/auth/profile") in authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const userModel = require("../models");
+const userModel = require("../models/userModel");
+const postModel = require("../models/postModel");
 const passport = require("passport");
 
 const authRoutes = new express.Router();
@@ -50,6 +51,46 @@ authRoutes.get("/auth/logout", (req, res) => {
   req.logout();
   req.flash("success_msg", "You are logged out");
   res.redirect("/auth/login");
+});
+
+authRoutes.get("/auth/profile", async (req, res) => {
+  try {
+    //const db = await connectToDB();
+    //const user = await db.collection('users').findOne({});
+    //const userId = "66a00711baa41bb0c54f3faa";
+    //const user = await userModel.findById(userId);
+    console.log("Profile route accessed");
+    
+    const userName = "hulk"; // Replace this with the desired user_name or fetch it dynamically
+    console.log("Searching for user with user_name:", userName);
+
+    const user = await userModel.findOne({ user_name: userName });
+    
+    console.log("User found:", user);
+
+    if (user) {
+      // Count total posts by user
+      const totalPosts = await postModel.countDocuments({ author: user.user_name });
+
+      // Find the latest post by user
+      const latestPost = await postModel.findOne({ author: user.user_name }).sort({ createdAt: -1 });
+
+      console.log("Total Posts:", totalPosts);
+      console.log("Latest Post:", latestPost);
+      res.render('profile', { 
+        title: "Profile", 
+        user: user,
+        totalPosts: totalPosts,
+        latestPost: latestPost
+      });
+    } else {
+      console.log("No user found");
+      res.status(404).send('No user found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = authRoutes;
