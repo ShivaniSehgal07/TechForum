c8d5f8a1c5ae2119731ab9886e136663fa0889f2
Shivani Sehgal <sehgalshivani07@gmail..com>
2024-07-29 20:52:12 -0400

    Fixed saerch by post

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,19 @@ const express = require("express");
 const authRoutes = require("./authRoutes");
 const postRoutes = require("./postRoutes");
 
+const homeRoutes = new express.Router()
+
 const getHome = (req, res) => res.render("index", { title: "TechForum - Home" });
-const homeRoute = new express.Router().get('/', getHome);
+homeRoutes.get('/', getHome);
+
+const getContactUs = (req, res) => res.render("contact-us", { title: "TechForum - Contact Us" });
+homeRoutes.get('/contact-us', getContactUs);
+
+const getAboutUs = (req, res) => res.render("about-us", { title: "TechForum - About Us" });
+homeRoutes.get('/about-us', getAboutUs);
 
 module.exports = {
   authRoutes,
-  homeRoute,
+  homeRoutes,
   postRoutes,
 }
